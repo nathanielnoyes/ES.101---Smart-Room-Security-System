9e37b615282b3e495328f956c9ede9f74f032fb9
nathanielnoyes <na.l.noyes@baypath.net>
2026-09-15 08:16:40 -0400

    Created the Blink and door test code

---
+++ BlinkSketch/SwichTest.ino
@@ -0,0 +1,11 @@
+void setup() {
+ // put your setup code here, to run once:
+ Serial.begin(9600);
+ pinMode(14,INPUT_PULLUP);
+}
+
+
+void loop() {
+ // put your main code here, to run repeatedly:
+ Serial.println(digitalRead(14));
+}
